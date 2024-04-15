0423288ee26e41d0c816404ecddc414fedc2a635
Nazneen <naz.chini@gmail.com>
2024-04-15 17:44:00 +0200

    feature: add modal for login/register forms

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -1,18 +1,34 @@
 import * as React from 'react'
 import {createRoot} from 'react-dom/client'
 import {Logo} from 'components/logo'
+import {Dialog} from '@reach/dialog'
+import '@reach/dialog/styles.css'
 
 function App() {
+  const [openModal, setOpenModal] = React.useState('none')
+
   return (
     <>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
       <div>
-        <button onClick={() => alert('login clicked!')}>Login</button>
+        <button onClick={() => setOpenModal('login')}>Login</button>
       </div>
       <div>
-        <button onClick={() => alert('register clicked!')}>Register</button>
+        <button onClick={() => setOpenModal('register')}>Register</button>
       </div>
+      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Login</h3>
+      </Dialog>
+      <Dialog aria-label="Register form" isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={() => setOpenModal('none')}>Close</button>
+        </div>
+        <h3>Register</h3>
+      </Dialog>
     </>
   )
 }
